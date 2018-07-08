175c75c8d7da632c6ba1c62934d66cd0dc25981c
latsiuhd <ldg283@gmail.com>
2018-07-08 16:13:41 +0200

    lesson 9 added second bean added xml name space for properties added mix of properties(getter setter) + constructor to bean

--- src/main/java/org/pantheon/Start.java
+++ src/main/java/org/pantheon/Start.java
@@ -8,5 +8,7 @@ public class Start {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         ModelT1000 t1000 = (ModelT1000) context.getBean("t1000");
         t1000.action();
+        ModelT1000 t1000Empty = (ModelT1000) context.getBean("t1000Empty");
+        t1000Empty.action();
     }
 }
