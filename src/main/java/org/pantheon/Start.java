de0e98e638552f7a12042cd991273de05d5c08ba
latsiuhd <ldg283@gmail.com>
2018-07-08 22:09:07 +0200

    lesson 12 added depends on database Bean

--- src/main/java/org/pantheon/Start.java
+++ src/main/java/org/pantheon/Start.java
@@ -8,14 +8,5 @@ public class Start {
         ApplicationContext context = new ClassPathXmlApplicationContext("all_context.xml");
         ModelT1000 t1000_1 = (ModelT1000) context.getBean("t1000");
         t1000_1.action();
-        System.out.println("********");
-
-        ModelT1000 t1000Empty_1 = (ModelT1000) context.getBean("t1000Empty");
-        ModelT1000 t1000Empty_2 = (ModelT1000) context.getBean("t1000Empty");
-        t1000Empty_1.action();
-
-        System.out.println("********* " + (t1000Empty_1 == t1000Empty_2));
-        System.out.println(t1000Empty_1);
-        System.out.println(t1000Empty_2);
     }
 }
